be592973770f16e072346f24b5a90b8773fd7bb4
zchen15 <zchen15@github.com>
2023-03-22 20:00:18 -0700

    tests

---
+++ tests/test_designer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# tests for designer.py
+
+import plasmid
+import glob
+
+def test_read_genbank():
+    # Read a genbank file
+    fname = 'data/dcas9_LacI.gb'
+    x = plasmid.read_genbank(fname)        
+    assert type(x).__name__ == 'Plasmid'
+    return x
+
+   
